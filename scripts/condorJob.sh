9cf5a360cf132fb81bb868728facaf6109d97344
Olmo Cerri <olmo.cerri@gmail.com>
2019-08-01 12:56:02 +0200

    Debug condor job

--- scripts/condorJob.sh
+++ scripts/condorJob.sh
@@ -3,4 +3,4 @@ source /afs/cern.ch/user/o/ocerri/.bash_profile
 
 cd /afs/cern.ch/user/o/ocerri/cernbox/JetAnomaly/scripts
 
-python ComputeExpected_pval.py -nBSM $1 -xsecBSM $2 --lumi $3
+python ComputeExpected_pval.py --nBSM $1 --xsecBSM $2 --lumi $3
